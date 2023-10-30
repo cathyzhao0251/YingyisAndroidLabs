95e85697f319dfb556b522ef1f31a58972114150
Yingyi Zhao <zhao0251@algonquinlive.com>
2023-10-30 12:37:45 -0400

    working recycler view

---
+++ app/src/main/java/algonquin/cst2335/zhao0251/data/MessageViewModel.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.zhao0251.data;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class MessageViewModel extends ViewModel {
+    public ArrayList<String> theMessages = new java.util.ArrayList<>();
+}
