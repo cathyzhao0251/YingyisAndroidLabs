4b66a740f85f58100095184ac3e021ee722ca723
Yingyi Zhao <zhao0251@algonquinlive.com>
2023-11-19 21:18:46 -0500

    Finish Week9 Toolbar

--- app/src/main/java/algonquin/cst2335/zhao0251/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/zhao0251/ChatRoom.java
@@ -3,6 +3,7 @@ package algonquin.cst2335.zhao0251;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
@@ -12,10 +13,13 @@ import androidx.room.Room;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -36,6 +40,7 @@ import algonquin.cst2335.zhao0251.databinding.ReceiveRowBinding;
 import algonquin.cst2335.zhao0251.databinding.SentRowBinding;
 
 public class ChatRoom extends AppCompatActivity {
+    private static final String FRAGMENT_TAG = "FRAGMENT_TAG";
 
     ActivityChatRoomBinding binding;
     ArrayList<ChatMessage> messages = null;
@@ -44,6 +49,12 @@ public class ChatRoom extends AppCompatActivity {
     private RecyclerView.Adapter myAdapter;
 
     ChatMessageDAO mDAO;
+    ChatMessage clickedMessage;
+    TextView globalMessageText;
+    int position;
+    MessageDetailsFragment chatFragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
 
     @Override
@@ -53,6 +64,7 @@ public class ChatRoom extends AppCompatActivity {
 
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
 
         chatModel = new ViewModelProvider(this).get(MessageViewModel.class);
         messages = chatModel.theMessages.getValue();
@@ -198,14 +210,14 @@ public class ChatRoom extends AppCompatActivity {
                         ChatMessage clickedMessage = messages.get(position);
                         chatModel.selectedmessages.postValue(clickedMessage);
 
-                        /*AlertDialog.Builder builder = new AlertDialog.Builder(ChatRoom.this);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ChatRoom.this);
                         builder.setMessage("Do you want to delete the message: " + messageText.getText())
                                 .setTitle("Question: ")
                                 .setPositiveButton("Yes", (dialog, cl) -> {
                                     Executor thread = Executors.newSingleThreadExecutor();
                                     thread.execute(() ->
                                     {
-                                        mDAO.deleteMessage(toDelete);
+                                        mDAO.deleteMessage(clickedMessage);
                                     });
 
                                     messages.remove(position);
@@ -216,20 +228,76 @@ public class ChatRoom extends AppCompatActivity {
                                                 Executor thread1 = Executors.newSingleThreadExecutor();
                                                 thread.execute(() ->
                                                 {
-                                                    mDAO.insertMessage(toDelete);
+                                                    mDAO.insertMessage(clickedMessage);
                                                 });
-                                                messages.add(position, toDelete);
+                                                messages.add(position, clickedMessage);
                                                 myAdapter.notifyDataSetChanged();
                                             })
                                             .show();
                                 })
                                 .setNegativeButton("No", (dialog, cl) -> {
                                 })
-                                .create().show();*/
+                                .create().show();
                     });
 
             messageText = itemView.findViewById(R.id.message);
             timeText = itemView.findViewById(R.id.time);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item_1:
+                String messageText = chatModel.selectedmessages.getValue().getMessage();
+                AlertDialog.Builder builder = new AlertDialog.Builder(ChatRoom.this);
+                builder.setMessage("Do you want to delete the message:" + messageText);
+                builder.setTitle("Question:")
+                        .setPositiveButton("Yes", (dialog, cl) -> {
+                            ChatMessage m = messages.get(position);
+
+                            Executor thread = Executors.newSingleThreadExecutor();
+                            thread.execute(() -> {
+                                mDAO.deleteMessage(clickedMessage);
+                            });
+
+                            messages.remove(position);
+                            myAdapter.notifyItemRemoved(position);
+                            removeFragment();
+                            Snackbar.make(globalMessageText, "You deleted message #" + position, Snackbar.LENGTH_LONG)
+                                    .setAction("Undo", clk2 -> thread.execute(() -> {
+                                        mDAO.insertMessage(clickedMessage);
+                                        messages.add(position, clickedMessage);
+                                        runOnUiThread(() -> myAdapter.notifyItemInserted(position));
+                                    }))
+                                    .show();
+                        })
+                        .setNegativeButton("No", (dialog, cl) -> {
+                        })
+                        .create().show();
+                break;
+
+            case R.id.item_2:
+                Toast.makeText(this, "Version 1.0. create by Yingyi Zhao", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value" + item.getItemId());
+        }
+        return true;
+    }
+
+    public void removeFragment() {
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+
         }
     }
 }
